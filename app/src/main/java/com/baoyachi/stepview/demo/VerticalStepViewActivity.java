75267c2b8dc620ec8665b60267eca55741758722
baoyachi <liaoymxsdl@sina.com>
2016-06-24 11:46:09 +0800

    add VerticalStepViewActivity

---
+++ app/src/main/java/com/baoyachi/stepview/demo/VerticalStepViewActivity.java
@@ -0,0 +1,20 @@
+package com.baoyachi.stepview.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 日期：16/6/24 11:41
+ * <p>
+ * 描述：垂直方向的StepView
+ */
+public class VerticalStepViewActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vertical_stepview);
+    }
+}
