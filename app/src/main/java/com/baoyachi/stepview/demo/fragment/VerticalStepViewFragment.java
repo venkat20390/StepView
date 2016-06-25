ab8dd60b63fa0e33c9b2a29cdece872fca00407b
baoyachi <liaoymxsdl@sina.com>
2016-06-25 16:47:27 +0800

    update description

--- app/src/main/java/com/baoyachi/stepview/demo/fragment/VerticalStepViewFragment.java
+++ app/src/main/java/com/baoyachi/stepview/demo/fragment/VerticalStepViewFragment.java
@@ -39,13 +39,7 @@ public class VerticalStepViewFragment extends Fragment
         mSetpview0 = (VerticalStepView) mView.findViewById(R.id.step_view0);
 
         List<String> list0 = new ArrayList<>();
-        list0.add("接已提交定案，等待系统确认");
-        list0.add("您的商品需要从外地调拨，我们会尽快处理，请耐心等待");
-        list0.add("您的订单已经进入亚洲第一仓储中心1号库准备出库");
-        list0.add("您的订单预计6月23日送达您的手中，618期间促销火爆，可能影响送货时间，请您谅解，我们会第一时间送到您的手中");
-        list0.add("您的订单已打印完毕");
-        list0.add("您的订单已拣货完成");
-        list0.add("扫描员已经扫描");
+        list0.add("您已提交定单，等待系统确认");
         list0.add("您的商品需要从外地调拨，我们会尽快处理，请耐心等待");
         list0.add("您的订单已经进入亚洲第一仓储中心1号库准备出库");
         list0.add("您的订单预计6月23日送达您的手中，618期间促销火爆，可能影响送货时间，请您谅解，我们会第一时间送到您的手中");
@@ -53,6 +47,10 @@ public class VerticalStepViewFragment extends Fragment
         list0.add("您的订单已拣货完成");
         list0.add("扫描员已经扫描");
         list0.add("打包成功");
+        list0.add("您的订单在京东【滑动外单分拣中心】发货完成，准备送往京东【北京通州分拣中心】");
+        list0.add("您的订单在京东【北京通州分拣中心】分拣完成");
+        list0.add("您的订单在京东【北京通州分拣中心】发货完成，准备送往京东【北京中关村大厦站】");
+        list0.add("您的订单在京东【北京通州分拣中心】验货完成，正在分配配送员");
         list0.add("配送员【包牙齿】已出发，联系电话【130-0000-0000】，感谢您的耐心等待，参加评价还能赢取好多礼物哦");
         list0.add("感谢你在京东购物，欢迎你下次光临！");
         mSetpview0.setStepsViewIndicatorComplectingPosition(list0.size() - 2)//设置完成的步数
