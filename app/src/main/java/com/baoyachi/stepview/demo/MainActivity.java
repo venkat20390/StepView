a2d1bbf11844f8279856cbc88978c6961df25ad2
baoyachi <liaoymxsdl@sina.com>
2016-07-06 18:13:39 +0800

    remove VerticalStepViewSnapshotFragment

--- app/src/main/java/com/baoyachi/stepview/demo/MainActivity.java
+++ app/src/main/java/com/baoyachi/stepview/demo/MainActivity.java
@@ -10,7 +10,6 @@ import com.baoyachi.stepview.demo.fragment.DrawCanvasFragment;
 import com.baoyachi.stepview.demo.fragment.HorizontalStepviewFragment;
 import com.baoyachi.stepview.demo.fragment.VerticalStepViewReverseFragment;
 import com.baoyachi.stepview.demo.fragment.VerticalStepViewFrowardFragment;
-import com.baoyachi.stepview.demo.fragment.VerticalStepViewSnapshotFragment;
 
 /**
  * 日期：16/6/22 16:01
@@ -46,7 +45,6 @@ public class MainActivity extends AppCompatActivity
         VerticalStepViewReverseFragment mVerticalStepViewFragment;
         DrawCanvasFragment mDrawCanvasFragment;
         HorizontalStepviewFragment mHorizontalStepviewFragment;
-        VerticalStepViewSnapshotFragment mVerticalStepViewSnapshotFragment;
         VerticalStepViewFrowardFragment mVerticalStepViewReverseFragment;
         FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
         int itemId = item.getItemId();
@@ -71,10 +69,6 @@ public class MainActivity extends AppCompatActivity
                 fragmentTransaction.replace(R.id.container, mVerticalStepViewReverseFragment).commit();
                 break;
 
-            case R.id.action_vertical_stepview_snapshot:
-                mVerticalStepViewSnapshotFragment = new VerticalStepViewSnapshotFragment();
-                fragmentTransaction.replace(R.id.container, mVerticalStepViewSnapshotFragment).commit();
-                break;
 
         }
         return super.onOptionsItemSelected(item);
