38458835b81f6394ffc4251569de520c20fe6f60
baoyachi <liaoymxsdl@sina.com>
2016-07-28 13:54:26 +0800

    add TestVerticalStepActivity

--- app/src/main/java/com/baoyachi/stepview/demo/MainActivity.java
+++ app/src/main/java/com/baoyachi/stepview/demo/MainActivity.java
@@ -70,6 +70,9 @@ public class MainActivity extends AppCompatActivity
             case R.id.action_test_horizontal_stepview:
                 startActivity(new Intent(this,TestHorizontalStepViewActivity.class));
                 break;
+            case R.id.action_test_vertical_stepview:
+                startActivity(new Intent(this,TestVerticalStepActivity.class));
+                break;
 
 
         }
